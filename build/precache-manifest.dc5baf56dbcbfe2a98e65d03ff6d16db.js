ec3f38c118da85052d61ff8cd02cb9d48db98d8c
deptrai <muelleimer9009@gmail.com>
2020-05-23 15:40:28 +0200

    init commit

---
+++ build/precache-manifest.dc5baf56dbcbfe2a98e65d03ff6d16db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "737749e37c02509bfce65e16bc1f219b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d7600bf1d17b7bd6842",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "74172e25ad151aa10372",
+    "url": "/static/js/2.867d7aad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.867d7aad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d7600bf1d17b7bd6842",
+    "url": "/static/js/main.c5004770.chunk.js"
+  },
+  {
+    "revision": "26ed4d9c4d4b8105600b",
+    "url": "/static/js/runtime-main.4d4e5bd2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
